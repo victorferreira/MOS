508a4de25b4019fa93d3db214bd54ae123ff7a99
Victor Ferreira <victor.araujo.ferreira@gmail.com>
2017-05-07 12:24:40 -0300

    new implementation of mos_read_line(), use getline() from stdio instead

--- src/main.c
+++ src/main.c
@@ -146,51 +146,51 @@ char **mos_split_line(char *line)
 }
 
 // version of mos_read_line using getline() on stdio.h
-// char *mos_read_line()
-// {
-//     char *line = NULL;
-//     ssize_t bufsize = 0; // have getline allocate a buffer for us.
-//     getline(&line, &bufsize, stdin);
-//     return line;
-// }
-
 char *mos_read_line()
 {
-    int bufsize = MOS_RL_BUFSIZE;
-    int position = 0;
-    char *buffer = malloc(sizeof(char) * bufsize);
-    int c;
-
-    if (!buffer) {
-        fprintf(stderr, "mos: allocation error\n");
-        exit(EXIT_FAILURE);
-    }
-
-    while (1) {
-        // Read a character
-        c = getchar();
-
-        // If we hit EOF, replace with a null character and return.
-        if (c == EOF || c == '\n') {
-            buffer[position] = '\0';
-            return buffer;
-        } else {
-            buffer[position] = c;
-        }
-        position++;
-
-        // If we exceeded the buffer, realocate
-        if (position >= bufsize) {
-            bufsize += MOS_RL_BUFSIZE;
-            buffer = realloc(buffer, bufsize);
-
-            if (!buffer) {
-                fprintf(stderr, "mos: allocation error\n");
-                exit(EXIT_FAILURE);
-            }
-        }
-    }
+    char *line = NULL;
+    ssize_t bufsize = 0; // have getline allocate a buffer for us.
+    getline(&line, &bufsize, stdin);
+    return line;
 }
+
+// char *mos_read_line()
+// {
+//     int bufsize = MOS_RL_BUFSIZE;
+//     int position = 0;
+//     char *buffer = malloc(sizeof(char) * bufsize);
+//     int c;
+
+//     if (!buffer) {
+//         fprintf(stderr, "mos: allocation error\n");
+//         exit(EXIT_FAILURE);
+//     }
+
+//     while (1) {
+//         // Read a character
+//         c = getchar();
+
+//         // If we hit EOF, replace with a null character and return.
+//         if (c == EOF || c == '\n') {
+//             buffer[position] = '\0';
+//             return buffer;
+//         } else {
+//             buffer[position] = c;
+//         }
+//         position++;
+
+//         // If we exceeded the buffer, realocate
+//         if (position >= bufsize) {
+//             bufsize += MOS_RL_BUFSIZE;
+//             buffer = realloc(buffer, bufsize);
+
+//             if (!buffer) {
+//                 fprintf(stderr, "mos: allocation error\n");
+//                 exit(EXIT_FAILURE);
+//             }
+//         }
+//     }
+// }
 
 void mos_loop(void)
 {
