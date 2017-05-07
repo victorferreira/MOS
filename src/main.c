c4ab2d9b68c47a5b5a3c05fe549da95fd99dd90a
Victor Ferreira <victor.araujo.ferreira@gmail.com>
2017-05-07 12:24:58 -0300

    new implementation of mos_read_line(), use getline() from stdio instead

--- src/main.c
+++ src/main.c
@@ -154,44 +154,6 @@ char *mos_read_line()
     return line;
 }
 
-// char *mos_read_line()
-// {
-//     int bufsize = MOS_RL_BUFSIZE;
-//     int position = 0;
-//     char *buffer = malloc(sizeof(char) * bufsize);
-//     int c;
-
-//     if (!buffer) {
-//         fprintf(stderr, "mos: allocation error\n");
-//         exit(EXIT_FAILURE);
-//     }
-
-//     while (1) {
-//         // Read a character
-//         c = getchar();
-
-//         // If we hit EOF, replace with a null character and return.
-//         if (c == EOF || c == '\n') {
-//             buffer[position] = '\0';
-//             return buffer;
-//         } else {
-//             buffer[position] = c;
-//         }
-//         position++;
-
-//         // If we exceeded the buffer, realocate
-//         if (position >= bufsize) {
-//             bufsize += MOS_RL_BUFSIZE;
-//             buffer = realloc(buffer, bufsize);
-
-//             if (!buffer) {
-//                 fprintf(stderr, "mos: allocation error\n");
-//                 exit(EXIT_FAILURE);
-//             }
-//         }
-//     }
-// }
-
 void mos_loop(void)
 {
     char *line;
